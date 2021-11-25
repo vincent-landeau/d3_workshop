8fc4e720daea2e53a3d777c574e7746eae517f7c
Vincent Landeau <vincent.landeau.contact@gmail.com>
2021-11-25 13:19:23 +0100

    added c3.js

---
+++ scripts/child.js
@@ -0,0 +1,18 @@
+d3.json("/data/pie.json", function(data) {
+    console.log(data);
+});
+
+var chart = c3.generate({
+    data: {
+        // iris data from R
+        columns: [
+            ['data1', 30],
+            ['data2', 120],
+        ],
+        type : 'pie',
+        onclick: function (d, i) { console.log("onclick", d, i); },
+        onmouseover: function (d, i) { console.log("onmouseover", d, i); },
+        onmouseout: function (d, i) { console.log("onmouseout", d, i); }
+    }
+});
+
